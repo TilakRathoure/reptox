a3e1c597bfdd972cc38376365944d710157a6e95
Tilak Rathoure <126514769+TilakRathoure@users.noreply.github.com>
2025-12-27 00:08:38 +0530

    add usememo

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,16 @@
-import { useState, useCallback } from "react";
-import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-const Child = React.memo(({ handle }) => {
-  console.log("Child rendering");
 
-  return (
-    <button onClick={handle}>
-      Hello
-    </button>
-  );
-});
-
-function Parent() {
-  const [count, setCount] = useState(0);
-
-  const handleclick = useCallback(() => {
-    console.log("child click");
-  }, []);
+const app=()=>{
 
   return (
-    <>
-      <div
-        onClick={() => setCount(count + 1)}
-        className="w-10 h-10 bg-yellow"
-      >
-        This is Parent component: {count}
-      </div>
+    <Router>
+      <Router>
+        <Route element={} path="/"/>
+      </Router>
+    </Router>
+  )
 
-      <Child handle={handleclick} />
-    </>
-  );
 }
 
-export default Parent;
+export default app;
